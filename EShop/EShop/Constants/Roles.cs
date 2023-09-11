eee23cacb81c3177b4032b81616a5cbbe5e8aa4f
sanjana <sanjana.sf3573>
2023-09-11 11:40:10 +0530

    main project is pushed by sanjana

---
+++ EShop/EShop/Constants/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Constants
+{
+   
+        public enum Roles
+        {
+            SuperAdmin,
+            Admin,
+            Basic
+        }
+    
+}
